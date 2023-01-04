5954acf3f09516dc3be2b35c12790a8daa59bc14
Jere Koivisto <jerekoivisto7@gmail.com>
2023-01-05 00:07:31 +0200

    Background scroll, player particles

---
+++ Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    [SerializeField] private Transform centerBackground;
+
+    private void Update()
+    {
+        if (transform.position.x >= centerBackground.position.x + 40.96f)
+        {
+            centerBackground.position = new Vector2(centerBackground.position.x, transform.position.y + 40.96f);
+        }
+
+        else if(transform.position.x >= centerBackground.position.x + 40.96f)
+        {
+            centerBackground.position = new Vector2(centerBackground.position.x, transform.position.y - 40.96f);
+        }
+    }
+}
