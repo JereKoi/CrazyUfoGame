4579c700b8ad75f31df24a9d83d218641dfd6cd2
Jere Koivisto <jerekoivisto7@gmail.com>
2023-01-05 21:51:06 +0200

    Asteroid spawn attempt

---
+++ Assets/Scripts/SpawnAsteroids.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAsteroids : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+    public float respawnTime = 1.0f;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(asteroidWave());
+    }
+
+    private void spawnAsteroid()
+    {
+        GameObject a = Instantiate(asteroidPrefab) as GameObject;
+        a.transform.position = new Vector2(screenBounds.x * -2, Random.Range(-screenBounds.y, screenBounds.y));
+    }
+    
+    IEnumerator asteroidWave()
+    {
+        while(true) // can change to boolean for example when game is ready to start from main menu
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnAsteroid();
+        }
+    }
+}
