727e842737d9401945d5cba3b2ba51ce03d14507
Jere Koivisto <jerekoivisto7@gmail.com>
2023-01-07 01:36:03 +0200

    Screen boundaries for player

--- Assets/Scripts/Boundaries.cs
+++ Assets/Scripts/Boundaries.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class Boundaries : MonoBehaviour
 {
-    private void Update()
+    public Camera MainCamera;
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    // Use this for initialization
+    void Start()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -4f, 4f), 
-            Mathf.Clamp(transform.position.y, -4f, 4f), transform.position.z);
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = viewPos;
     }
 }
