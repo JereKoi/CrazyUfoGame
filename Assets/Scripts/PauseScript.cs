ce058fde6d0c9d132eea92867097018a2c541697
Jere Koivisto <jerekoivisto7@gmail.com>
2023-01-11 20:41:51 +0200

    UI

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    public Button pauseButton;
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+    }
+}
--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -10,7 +10,7 @@ public class ScrollingBackground : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(speedup());
+        //StartCoroutine(speedup());
     }
 
     // Update is called once per frame
@@ -19,25 +19,25 @@ public class ScrollingBackground : MonoBehaviour
         bgRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
     }
 
-    IEnumerator speedup()
-    {
-        while (true)
-        {
-            if (speed <= 1.0f)
-            {
-                yield return new WaitForSeconds(2);
-                speed += 0.01f;
-            }
-            else if (speed >= 1.0f)
-            {
-                speed = 1.0f;
-            }
-        }
+    //IEnumerator speedup()
+    //{
+    //    while (true)
+    //    {
+    //        if (speed <= 1.0f)
+    //        {
+    //            yield return new WaitForSeconds(2);
+    //            speed += 0.01f;
+    //        }
+    //        else if (speed >= 1.0f)
+    //        {
+    //            speed = 1.0f;
+    //        }
+    //    }
 
         //while (true)
         //{
         //    yield return new WaitForSeconds(2);
         //    speed += 0.01f;
         //}
-    }
+    //}
 }
